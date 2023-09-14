2a2caea2440f4add20f3d63676d65cc8dc09282e
gustavocarneiro453 <gustavocidc@gmail.com>
2023-09-14 15:36:05 +0000

    Atividade em andamento

---
+++ CardGame/Inventario.java
@@ -0,0 +1,14 @@
+package CardGame;
+
+public class Inventario {
+    private double [] cartas = new double [200];
+    private int nivel;
+    private int cardcoins;
+}
+
+public Inventario() {
+    this.cartas = cartas;
+    this.nivel = 1; // Nível inicial
+    this.cardcoins = 0; // Quantidade inicial de cardcoins
+}
+
---
+++ CardGame/Usuario.java
@@ -0,0 +1,22 @@
+package CardGame;
+
+public class Usuario {
+    private String user;
+    private String CPF;
+    private String senha;
+    private int idade;
+    private char sexo;
+    private String email;
+    private int nivel;
+    
+    public Usuario(String user, String CPF, String senha, int idade, char sexo, String email, int nivel) {
+        this.user = user;
+        this.CPF = CPF;
+        this.senha = senha;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.email = email;
+        this.nivel = nivel;
+
+}
+
